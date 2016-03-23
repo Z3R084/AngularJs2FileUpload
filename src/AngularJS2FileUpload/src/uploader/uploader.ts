f95b76a0af3f87d95034268fddabd60ca72c41be
Christian Quast <christian.quast84@googlemail.com>
2016-03-23 22:11:45 +0100

    added change event to file input

--- src/AngularJS2FileUpload/src/uploader/uploader.ts
+++ src/AngularJS2FileUpload/src/uploader/uploader.ts
@@ -2,14 +2,17 @@
 import {NgFor} from 'angular2/common';
 import {FileUploader} from './file-uploader';
 
-@Component({
+@Component( {
 	selector: 'qdt-uploader',
-	template: '<div><input type="file" id="fileUpload" multiple class="btn btn-default" [uploader]="uploader" /></div>' +
-	'<div *ngFor="#file in uploader.queue" style="margin-top: 20px; border-bottom-color: antiquewhite; border-bottom-style: double;">' +
-	'<div><span>{{ file.name }}</span></div>' +
-	'</div>'
+	template: '<div><input type="file" id="fileUpload" multiple class="btn btn-default" (change)="changeEvent( $event )" /></div>'
+	//'<div *ngFor="#file in files" style="margin-top: 20px; border-bottom-color: antiquewhite; border-bottom-style: double;">' +
+	//'<div><span>{{ file.name }}</span></div>' +
+	//'</div>'
 })
 
 export class QdtUploader {
-
+	files: Array<any> = [];
+	changeEvent( $event ): void {
+		console.log( $event.target.files );
+	}
 }
