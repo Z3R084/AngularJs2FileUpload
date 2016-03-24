c7febc6ba173ba1dbd34df3922ec765fa32ec20e
christian.quast84@googlemail.com <christian.quast84@googlemail.com>
2016-03-24 15:38:34 +0100

    updated fileUploader and uploader class

--- src/AngularJS2FileUpload/src/uploader/file-uploader.ts
+++ src/AngularJS2FileUpload/src/uploader/file-uploader.ts
@@ -1,8 +1,10 @@
-﻿export class FileUploader {
+﻿import {FileItem} from './file-item';
+
+export class FileUploader {
 	public queue: Array<any> = [];
 	public filters: Array<any> = [];
 
-	public addToQueue(files: any[], options: any, filters: any) {
+	public addToQueue(files: any[]) { //, options: any, filters: any
 		let list: any[] = [];
 		for (let file of files) {
 			list.push(file);
@@ -11,8 +13,9 @@
 		let addedFileItems: any[] = [];
 
 		list.map(some => {
-			addedFileItems.push(some);
-			this.queue.push(some);
+			let fileItem = new FileItem(some);
+			//addedFileItems.push(some);
+			this.queue.push(fileItem);
 		});
 	}
 }
