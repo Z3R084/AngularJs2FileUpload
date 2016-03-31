be1f3aa2f75c5410240c8aba1ff07cd742e88a76
christian.quast84@googlemail.com <christian.quast84@googlemail.com>
2016-03-31 16:07:43 +0200

    added progress bar

--- src/AngularJS2FileUpload/src/uploader/uploader.ts
+++ src/AngularJS2FileUpload/src/uploader/uploader.ts
@@ -34,7 +34,11 @@ import {FileUploader} from './file-uploader';
 			<tr *ngFor="#file of fileUploader.queue">
 				<td>{{ file._file.name }}</td>
 				<td>{{ file._file.humanSize }}</td>
-				<td>{{ url }}</td>
+				<td>
+					<div class="progress" style="margin-bottom: 0; width: 200px;">
+						<div class="progress-bar" role="progressbar" [ngStyle]="{ 'width': item.progress + '%' }"></div>
+					</div>
+				</td>
 				<td>
 					<button class="btn btn-primary btn-xs" (click)="file.upload()"><span class="glyphicon glyphicon-upload"></span> Upload</button>&nbsp;
 					<button class="btn btn-danger btn-xs" (click)="file.remove()"><span class="glyphicon glyphicon-trash"></span> Delete</button>
